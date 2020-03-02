701287fe10fffa9ede95b501516780412cdd2b70
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-02 12:00:44 -0800

    Adds matrices of my board, ready to create rules

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,44 @@
-var boxes = document.getElementsByTagName('th');
-console.log(boxes);
-for(var i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener('click', function(){console.log('X')})
+// var boxes = document.getElementsByClassName('box');
+// console.log(boxes);
+
+// var player1 = 'X';
+// var player2 = 'O'
+
+//boolean to determin what plays next
+var playX = false;
+
+//board storage to eventually run a winner on
+var board = [['','',''],['','',''],['','','']];
+console.log(board);
+
+//change playX everytime play is called
+var play = function(){
+  //if not playX
+  if(!playX) {
+    //change it for the next turn
+    playX = true;
+    return 'X'
+  } else {
+    playX = false;
+    return 'O'
+  }
 }
+
+for(var i = 0; i <= 8; i++) {
+  var box = document.getElementById(i);
+  box.addEventListener('click', function(){
+    if(!event.target.innerHTML) {
+      (event.target).append(play());
+    }
+    // console.log(document.getElementById(event.target.id).parentElement.className);
+    // console.log(event.target.id);
+    board[document.getElementById(event.target.id).parentElement.className][event.target.className] = event.target.innerHTML;
+    console.log(board);
+    //eventually after every play() we need to check for a winner
+    //if board is full and no winner return a tie
+  })
+}
+
+//function to check for winner
+
 console.log('hello from app.js');
