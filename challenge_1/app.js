1a0e5293044744c23a2b15e8e3a4d2a9af904260
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-02 10:26:26 -0800

    Adds a 3x3 table and eventListener for each box

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,6 @@
-
-
+var boxes = document.getElementsByTagName('th');
+console.log(boxes);
+for(var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', function(){console.log('X')})
+}
+console.log('hello from app.js');
