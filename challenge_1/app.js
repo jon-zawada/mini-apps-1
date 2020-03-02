f4c8fb3b1063aea46a032663478d96c99c26fa4a
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-02 12:20:44 -0800

    Adds funcions to check for a winner by row and column NOT diagonal yet

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -6,6 +6,7 @@
 
 //boolean to determin what plays next
 var playX = false;
+var gameWon = false;
 
 //board storage to eventually run a winner on
 var board = [['','',''],['','',''],['','','']];
@@ -34,11 +35,34 @@ for(var i = 0; i <= 8; i++) {
     // console.log(event.target.id);
     board[document.getElementById(event.target.id).parentElement.className][event.target.className] = event.target.innerHTML;
     console.log(board);
+    // rowWinner();
+    columnWinner();
     //eventually after every play() we need to check for a winner
     //if board is full and no winner return a tie
   })
 }
 
+var rowWinner = function(){
+  // console.log('hi from row winner')
+  for(var i = 0; i < board.length; i++) {
+    if(board[i][0] !== '' && board[i][1] !== '' && board[i][2] !== '' &&
+      board[i][0] === board[i][1] && board[i][0] === board[i][2] && board[i][1] === board[i][2]) {
+      gameWon = true;
+      alert('WE HAVE A WINNER');
+    }
+  }
+}
+
+var columnWinner = function(){
+  for(var i = 0; i < board.length; i++) {
+    if(board[0][i] !== '' && board[1][i] !== '' && board[2][i] !== ''
+      && board[0][i] === board[1][i] && board[0][i] === board[2][i] && board[1][i] === board[2][i]
+    ){
+      gameWon = true;
+      alert('WE HAVE A WINNER');
+    }
+  }
+}
 //function to check for winner
 
 console.log('hello from app.js');
