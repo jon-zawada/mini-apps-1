c780358b59849cdbe35cf4c1f2d5334e09a7bdeb
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-03 15:01:21 -0800

    Adds general structure with no working posts or gets

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,4 @@
+var text = document.getElementById('textarea').innerHTML
+//
 
-
+module.exports = text;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,10 +2,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const text = require('./client/app.js')
 
 // app.get('/', (req, res) => res.send('Hello World!!!!!!!!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.use('/', express.static('client'))
 
-// app.post('/upload_json', (req, res) => {})
+app.post('/upload_json', (req, res) => {
+  console.log(req.body)
+  console.log('HELLO FROM SERVER.JS')
+  res.send(text)
+})
+
+// app.get('/upload_json', (req, res) =>{})
