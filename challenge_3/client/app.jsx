1199af39770e973c0ab111bf15b66902c30168d1
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-04 13:33:12 -0800

    Adds environment

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,6 @@
 
+var App = () => (
+  <div>Hello from App.jsx</div>
+)
 
+// export default App;
---
+++ challenge_3/public/index.js
@@ -0,0 +1,3 @@
+import App from '../client/app.jsx';
+
+ReactDom.render(<App/>, document.getElementById('app'));
